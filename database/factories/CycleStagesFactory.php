f21e8f13d805b9fa2b22e391dada03f93bf69157
Mu'adz <59911607+MuadzMuadz@users.noreply.github.com>
2025-05-07 09:32:03 +0700

    adding_factory for filed feature

--- database/factories/CycleStagesFactory.php
+++ database/factories/CycleStagesFactory.php
@@ -2,22 +2,24 @@
 
 namespace Database\Factories;
 
+use App\Models\Cycle;
+use App\Models\CycleStages;
+use App\Models\GrowStages;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CycleStages>
  */
-class CycleStagesFactory extends Factory
+class CycleStageFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = CycleStages::class;
+
     public function definition(): array
     {
         return [
-            //
+            'cycle_id' => Cycle::factory(),
+            'grow_stage_id' => GrowStages::factory(),
+            'started_at' => $this->faker->dateTime,
         ];
     }
 }
